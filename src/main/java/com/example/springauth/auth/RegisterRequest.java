80b14d01f347be617b7fa1a5b960cc8c215a9056
mustaphamajed <mustapha.majed@esprit.tn>
2024-03-23 03:21:14 +0100

    feat(auth): Add request

---
+++ src/main/java/com/example/springauth/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.example.springauth.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname , lastname ,email ,password;
+
+}
